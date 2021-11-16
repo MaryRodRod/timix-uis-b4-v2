bcc6787b98054e037f60cf3f6c4974e678a84bea
EdwardJanR <ejobledop@gmail.com>
2021-11-16 07:27:42 -0500

    📝 Create first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddMusicaComponent } from './add-musica/add-musica.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMusicaComponent
   ],
   imports: [
     BrowserModule,
